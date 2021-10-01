3514e0e2658937d4f339b473170bebd478a1348e
Mister-Ndiaye <albathie97@gmail.com>
2021-10-01 07:33:42 +0000

    initial commit

---
+++ src/main/java/com/tdsi/sn/app_moblile_api/AppMoblileApiApplication.java
@@ -0,0 +1,13 @@
+package com.tdsi.sn.app_moblile_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMoblileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMoblileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdsi/sn/app_moblile_api/AppMoblileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdsi.sn.app_moblile_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoblileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
