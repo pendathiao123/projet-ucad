7e1112e8ae094a7b9e6f6edeaac4c72cbfbb9e0b
massamba sow <massambasow4@gmail.com>
2021-10-23 23:19:46 +0000

    add by massamba

--- src/main/java/com/tdsi/sn/app_moblile_api/Controller/ControlleurController.java
+++ src/main/java/com/tdsi/sn/app_moblile_api/Controller/ControlleurController.java
@@ -26,7 +26,7 @@ public class ControlleurController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello massamba";
+        return "hello massamba sowghhuiiii";
     }
 
     @GetMapping("/controlleurs")
