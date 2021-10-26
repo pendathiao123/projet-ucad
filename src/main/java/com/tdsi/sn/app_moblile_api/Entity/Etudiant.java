01a37028f3b8c929722c9ff5dab909b6508a2699
massamba sow <massambasow4@gmail.com>
2021-10-26 00:33:49 +0000

    add by mass

--- src/main/java/com/tdsi/sn/app_moblile_api/Entity/Etudiant.java
+++ src/main/java/com/tdsi/sn/app_moblile_api/Entity/Etudiant.java
@@ -23,5 +23,5 @@ public class Etudiant {
     private int motPasse ;
     private  int solde ;
     private String numero_carte ;
-    private BigInteger pu = BigInteger.valueOf(0);
+    private BigInteger pu;
 }
