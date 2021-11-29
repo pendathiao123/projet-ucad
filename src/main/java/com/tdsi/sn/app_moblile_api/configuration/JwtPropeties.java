b230e4c7e0eaf2185b999ca64e201223b5b9a6eb
massamba sow <massambasow4@gmail.com>
2021-11-29 20:33:25 +0000

    vrai api

--- src/main/java/com/tdsi/sn/app_moblile_api/configuration/JwtPropeties.java
+++
@@ -1,9 +0,0 @@
-package com.tdsi.sn.app_moblile_api.configuration;
-
-public class JwtPropeties {
-
-    public static final String secret  = "secret pour mobile ucad";
-    public static final int EXPIRATION_TIME = 864_000_000;
-    public static final String TOKEN_PREFIX = "Bearer";
-    public static final String HEADER_STRING = "Authorization";
-}
