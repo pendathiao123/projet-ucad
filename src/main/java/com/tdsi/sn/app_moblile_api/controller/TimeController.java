19366045260506a47a0edeeebe306146694e5d62
Penda Thiao <thiaopenda2503@gmail.com>
2021-12-07 19:02:45 +0000

    first commit

---
+++ src/main/java/com/tdsi/sn/app_moblile_api/controller/TimeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/api/v1.0")
+public class TimeController {
+
+    @GetMapping("/time")
+    @ResponseStatus(HttpStatus.OK)
+    public String getCurrentTime() {
+
+        int a = 6;
+        return Instant.now().toString();
+    }
+}
--- src/main/java/com/tdsi/sn/app_moblile_api/repository/PavillonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tdsi.sn.app_moblile_api.repository;
-
-import com.tdsi.sn.app_moblile_api.entity.Pavillon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PavillonRepository extends JpaRepository<Pavillon,Integer> {
-}
